cbfa8bfca33689dabc1ab1c0be0fbeee444dad42
Julia Fariga <45482921+Mipside@users.noreply.github.com>
2019-07-31 16:07:19 +0300

    Servlets2: AccessFilter improvement

--- src/main/java/com/fariga/filter/AccessFilter.java
+++ src/main/java/com/fariga/filter/AccessFilter.java
@@ -1,7 +1,5 @@
 package com.fariga.filter;
 
-import lombok.extern.log4j.Log4j;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -9,7 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@Log4j
 @WebFilter(urlPatterns = {"/user/"})
 public class AccessFilter implements Filter {
 
@@ -21,18 +18,14 @@ public class AccessFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-
         String uri = request.getRequestURI();
         HttpSession session = request.getSession();
         String userRole = (String) session.getAttribute("UserRole");
-        if (uri.endsWith("/user/login/") && (userRole.equals("Admin"))) {
+        if (uri.endsWith("/user/login/") && ((userRole.equals("Admin")) | (userRole.equals("User")))) {
             filterChain.doFilter(request, response);
-        } else if (uri.endsWith("/user/login/") && (userRole.equals("null"))) {
-            request.getRequestDispatcher("/WEB-INF/user/login/login.jsp").forward(request, response);
         } else {
             // pass the request along the filter chain
-            filterChain.doFilter(request, response);
-            log.info("Access filter: session is NOT Null because there is a user");
+            request.getRequestDispatcher("/WEB-INF/user/login/login.jsp").forward(request, response);
         }
     }
 
